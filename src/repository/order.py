1b8df39e8f4665cd494c6205be7d6218d850d82c
Maditek <tr.kops1@gmail.com>
2025-07-21 20:25:26 +0000

    feat(user): added repository

--- src/repository/order.py
+++ src/repository/order.py
@@ -4,12 +4,12 @@ from sqlmodel import Field, SQLModel
 from src.repository.enums import OrderStatus
 
 
-class OrdersBase(SQLModel):
+class OrderBase(SQLModel):
     customer_name: str
     user_id: int = Field(foreign_key="users.id")
 
 
-class Orders(OrdersBase, table=True):
+class Order(OrderBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
     created_at: datetime = Field(default_factory=datetime.datetime.now)
     status: OrderStatus = Field(default=OrderStatus.PENDING)
