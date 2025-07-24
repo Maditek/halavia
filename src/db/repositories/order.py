d54e1d598d724e9b7a6efe462f219874016eb472
Maditek <tr.kops1@gmail.com>
2025-07-24 20:41:47 +0000

    feat(order): updated repository

--- src/db/repositories/order.py
+++ src/db/repositories/order.py
@@ -1,5 +1,6 @@
-from sqlmodel import Session
+from sqlmodel import Session, select
 
+from src.db.enums import OrderStatus
 from src.db.tables.order import Order
 
 
@@ -16,8 +17,18 @@ class OrderRepository:
         self.session.refresh(order)
         return order
 
-    def update(self, order: Order) -> Order:
+    def update_status(self, order: Order, status: OrderStatus) -> Order:
+        order.status = status
         self.session.add(order)
         self.session.commit(order)
         self.session.refresh(order)
         return order
+
+    def delete(self, order: Order) -> None:
+        self.session.delete(order)
+        self.session.commit()
+
+    def get_by_user_id(self, user_id: int) -> list[Order]:
+        statement = select(Order).where(Order.user_id == user_id)
+        orders = self.session.exec(statement).all()
+        return orders
