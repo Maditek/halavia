bb57416e29af75cf4c63a1812808b8f13ca27a65
Maditek <tr.kops1@gmail.com>
2025-07-25 09:08:52 +0000

    feat(order): added custom exception

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 from contextlib import asynccontextmanager
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from src.core.exceptions.order import OrderNotFoundException
 from src.db.init.db import create_db_and_tables
 
 
@@ -10,3 +12,13 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+
+
+@app.exception_handler(OrderNotFoundException)
+def order_not_found_handler(request: Request, exc: OrderNotFoundException):
+    return JSONResponse(
+        status_code=404,
+        content={
+            f"error": f"{exc.message}",
+        },
+    )
